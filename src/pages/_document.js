0ba3a8a00825fe07c5127bcb45144d6cedd625d2
administrador23 <87953444+WilliamCipriani@users.noreply.github.com>
2023-11-15 23:14:34 -0500

    se agrego Head a document

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="es">
-      <Head />
+      <Head >
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <body>
         <Main />
         <NextScript />
